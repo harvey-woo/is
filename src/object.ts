501765781b71aab9bff808fbbf367557d493df36
Harvey.Woo <harvey.woo@outlook.com>
2018-11-06 18:45:19 +0800

    chroe: add some docs

--- src/object.ts
+++ src/object.ts
@@ -1,12 +1,24 @@
+/**
+ * detemine if target is a plain object
+ * @param target 
+ */
 export function isPlainObject(target: any): boolean {
   if (!target) return false
   return Object.getPrototypeOf(target) === Object.prototype || Object === target.constructor
 }
 
+/**
+ * detemine if target is object-like such as function
+ * @param target 
+ */
 export function isObjectLike(target: any): boolean {
   return typeof target === 'object' || typeof target === 'function'
 }
 
+/**
+ * detemine if target is a global object, for example: window, global
+ * @param target 
+ */
 export function isGlobal(target: any): boolean {
   return target && typeof target === 'object' && typeof target.setTimeout === 'function'
 }
